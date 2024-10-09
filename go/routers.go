ddbe7e7b4cf106c0b9e8b3a0937a49ad5c1c1a6f
Shrinjay Mukherjee <shrinjay@opal.dev>
2024-10-09 11:47:07 -0400

    add signing key check

--- go/routers.go
+++ go/routers.go
@@ -10,21 +10,29 @@
 package openapi
 
 import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 )
 
 // Route is the information for every URI.
 type Route struct {
 	// Name is the name of this Route.
-	Name		string
+	Name string
 	// Method is the string for the HTTP method. ex) GET, POST etc..
-	Method		string
+	Method string
 	// Pattern is the pattern of the URI.
-	Pattern	 	string
+	Pattern string
 	// HandlerFunc is the handler function of this route.
-	HandlerFunc	gin.HandlerFunc
+	HandlerFunc gin.HandlerFunc
 }
 
 // NewRouter returns a new router.
@@ -34,6 +42,8 @@ func NewRouter(handleFunctions ApiHandleFunctions) *gin.Engine {
 
 // NewRouter add routes to existing gin engine.
 func NewRouterWithGinEngine(router *gin.Engine, handleFunctions ApiHandleFunctions) *gin.Engine {
+	router.Use(validateOpalSignature(os.Getenv("OPAL_SIGNING_SECRET")))
+
 	for _, route := range getRoutes(handleFunctions) {
 		if route.HandlerFunc == nil {
 			route.HandlerFunc = DefaultHandleFunc
@@ -73,7 +83,7 @@ type ApiHandleFunctions struct {
 }
 
 func getRoutes(handleFunctions ApiHandleFunctions) []Route {
-	return []Route{ 
+	return []Route{
 		{
 			"AddGroupResource",
 			http.MethodPost,
@@ -170,5 +180,77 @@ func getRoutes(handleFunctions ApiHandleFunctions) []Route {
 			"/users",
 			handleFunctions.UsersAPI.GetUsers,
 		},
+	}
+}
+
+// GenerateSignature generates a signature for a given payload in a HTTP request
+func GenerateSignature(
+	signingSecret string,
+	timestamp string,
+	serializedBlob []byte,
+) (string, error) {
+	// Concatenate base string
+	sigBaseString := "v0:" + timestamp + ":" + string(serializedBlob)
+
+	// Hash base string to get signature
+	hash := hmac.New(sha256.New, []byte(signingSecret))
+	_, err := hash.Write([]byte(sigBaseString))
+	if err != nil {
+		return "", errors.Wrap(err, "error writing hash")
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+func validateOpalSignature(signingSecret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		opalSignature := c.GetHeader("X-Opal-Signature")
+		if opalSignature == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, &Error{
+				Code:    http.StatusUnauthorized,
+				Message: "X-Opal-Signature header is missing",
+			})
+			return
+		}
+		opalRequestTimestamp := c.GetHeader("X-Opal-Request-Timestamp")
+		if opalRequestTimestamp == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, &Error{
+				Code:    http.StatusUnauthorized,
+				Message: "X-Opal-Request-Timestamp header is missing",
+			})
+			return
+		}
+
+		var bodyStr string
+		// Read request body, once the request body is read, it cannot be read again
+		// so we need to save it in a variable and then reassign it to the Request.Body
+		var bodyBytes []byte
+		var err error
+		if c.Request.Body != nil {
+			bodyBytes, err = ioutil.ReadAll(c.Request.Body)
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusInternalServerError, &Error{
+					Code:    http.StatusInternalServerError,
+					Message: "Unable to read request body",
+				})
+				return
+			}
+			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+			bodyStr = strings.TrimSpace(string(bodyBytes))
+		}
+		if bodyStr == "" {
+			bodyStr = "{}"
+		}
+
+		signature, err := GenerateSignature(signingSecret, opalRequestTimestamp, []byte(bodyStr))
+		if signature != opalSignature || err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, &Error{
+				Code:    http.StatusUnauthorized,
+				Message: "Invalid signature",
+			})
+			return
+		}
+
+		c.Next()
 	}
 }
